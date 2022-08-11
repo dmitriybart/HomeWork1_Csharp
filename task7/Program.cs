fe29c9d2fb4880bf942c3b2a36b9eee961a406d6
Dmitriy B <kupperfeet@gmail.com>
2022-08-11 12:37:35 +0300

    task 7 done

---
+++ task7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int userNumber = int.Parse (Console.ReadLine());
+int programNumber = userNumber/10;
+int secondNumber = programNumber%10;
+Console.WriteLine(secondNumber + " - это вторая цифра в Вашем числе");
