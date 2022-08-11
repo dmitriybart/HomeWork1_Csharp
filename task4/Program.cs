e4138cafe4f001db64c5896710254ca611701fb1
Dmitriy B <kupperfeet@gmail.com>
2022-08-11 12:00:23 +0300

    task 4 done

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите любое число: ");
+double number = double.Parse(Console.ReadLine());
+
+if (number%2==0)
+{
+    Console.WriteLine(number + " - это число чётное");
+}
+else 
+{
+    Console.WriteLine(number + " - это число нечётное");
+
+}
