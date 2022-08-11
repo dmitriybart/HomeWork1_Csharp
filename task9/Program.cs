08de7470641b8fe2e23803c1b02e27a919a370e3
Dmitriy B <kupperfeet@gmail.com>
2022-08-11 13:08:09 +0300

    task 9 done

---
+++ task9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int userNumber = int.Parse(Console.ReadLine());
+int numberC = userNumber%10;
+int numberF = userNumber-numberC;
+int numberB = numberF%100;
+int numberA = userNumber-numberC-numberB;
+int programNumber = numberA/10;
+int doneNumber = programNumber+numberC;
+Console.Write("Число без второй цифры Вашего числа: " + doneNumber);
