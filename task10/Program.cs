0b63004b08adb0c12ab7f466c51a2438b38fe503
Dmitriy B <kupperfeet@gmail.com>
2022-08-11 13:21:08 +0300

    task 10 done

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите число: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Задайте кратность: ");
+int multipleNumber = int.Parse(Console.ReadLine());
+
+if (userNumber%multipleNumber==0)
+{
+    Console.Write("Число " + userNumber + " кратно числу " + multipleNumber ); 
+}
+else
+{
+
+Console.WriteLine("Число " + userNumber + " не кратно числу "  + multipleNumber + ", остаток равен: " + userNumber%multipleNumber);
+}
