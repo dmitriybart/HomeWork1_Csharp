166e125b4b9dd105d61a14949b57e150563051eb
Dmitriy B <kupperfeet@gmail.com>
2022-08-11 12:24:16 +0300

    task 6 done

---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int userNumber = int.Parse(Console.ReadLine());
+int threeNumber = userNumber%10;
+Console.WriteLine(threeNumber + " - это последняя цифра Вашего числа");
