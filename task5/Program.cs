0c16dd8cc74461e2db5ac8858e650d30111d9c81
Dmitriy B <kupperfeet@gmail.com>
2022-08-11 12:14:09 +0300

    task 5 done

---
+++ task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введите начало интервала(отрицательное число): ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Введите конец интервала: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+while (firstNumber < secondNumber+1)
+{
+    Console.Write(firstNumber + " ");
+    firstNumber++;
+}
+
