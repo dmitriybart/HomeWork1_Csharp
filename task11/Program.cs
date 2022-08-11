b3aaa210b196d14593978571b20bf7daf250dbbc
Dmitriy B <kupperfeet@gmail.com>
2022-08-11 13:38:49 +0300

    task 11 done

---
+++ task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число меньше 1000: ");
+int userNumber = int.Parse (Console.ReadLine());
+if (userNumber < 100)
+{
+    Console.Write("Третьей цифры в числе нет!");
+    
+}
+else
+{
+    int programNumber = userNumber%10;
+    Console.Write("Третья цифра числа " + userNumber + " равна " + programNumber);
+}
+
